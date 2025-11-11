f1a9f2062fb5b05aeb2c0292a14a9d5b607a13a7
gaaab04 <gabriele.francescato@studenti.unitn.it>
2025-11-11 20:04:27 +0100

    Aggiunte variabii colore
    Ho aggiunto un file colors.css per avere delle variabili per i colori in modo sia più facile cambiare lo stile generale in caso di cambiamenti

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import InputMessage from "../components/InputMessage";
+import "../styles/MainPage.css"
 
 function MainPage() {
     return (
