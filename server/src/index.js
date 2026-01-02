13008326314f5559aeeed5abe9bc1543fc45a44d
gaaab04 <gabriele.francescato@studenti.unitn.it>
2026-01-02 19:07:45 +0100

    fix test per deploy pt6

--- server/src/index.js
+++ server/src/index.js
@@ -30,14 +30,25 @@ const allowedOrigins = process.env.NODE_ENV === 'production'
     : ['http://localhost:5173'];
 
 app.use(cors({
-    origin: allowedOrigins,
-    credentials: true
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+        return callback(new Error('Not allowed by CORS'));
+    },
+    credentials: true,
 }));
 
 app.options('*', cors({
-   origin: allowedOrigins,
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) return callback(null, true);
+        return callback(new Error('Not allowed by CORS'));
+    },
     credentials: true,
-}))
+}));
+
 
 serviceStatusManager.initialize();
 
